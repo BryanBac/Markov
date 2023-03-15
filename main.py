54409e3a858186734cd0afa6984743193f345e35
BryanBac <bebac@correo.url.edu.gt>
2023-03-15 12:59:17 -0600

    definición de iteraciones como variable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 from markov_functions import markov
 
-matriz = np.array([[0.8, 0.1, 0.1], [0.03, 0.95, 0.02], [0.2, 0.05, 0.75]])
-vector = np.array([0.45, 0.25, 0.3])
+matriz = np.array([[0.52, 0.3, 0.18], [0.23, 0.35, 0.42], [0.07, 0.24, 0.69]])
+vector = np.array([0, 1, 0])
+iteraciones = 3
 print("Aquí están las probabilidades resultantes \n")
-print(markov(vector, matriz, 2))
+print(markov(vector, matriz, iteraciones))
